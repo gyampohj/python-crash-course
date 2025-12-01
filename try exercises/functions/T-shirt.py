8b45b9c95d1792c9ff9a67fb78020477af8b2ced
gyampohj <gyampohj53@gmail.com>
2025-12-01 16:14:23 +0000

    t shirt exercise solved for default and large size

---
+++ try exercises/functions/T-shirt.py
@@ -0,0 +1,5 @@
+def make_shirt(size, large,):
+    print(f"\nI love python is currently printed on {large} size")
+    print(f"{size.title()} size is currently available to purchase")
+
+make_shirt(size= 'small', large='large')
---
+++ try exercises/functions/messages.py
@@ -0,0 +1,5 @@
+#Write a function called display_message() that prints one sentence telling everyone what you are learning about in this chapter.Call the function, and make sure the message displays correctly.
+
+def message():
+    print(f"I am learning functions")
+message()
