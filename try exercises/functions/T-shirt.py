acf6c14887033e236794de520b9aa736347b5e5c
gyampohj <gyampohj53@gmail.com>
2025-12-01 16:17:45 +0000

    inclusion of a medium size shirt to complete the t shirt exercise

--- try exercises/functions/T-shirt.py
+++ try exercises/functions/T-shirt.py
@@ -1,5 +1,6 @@
-def make_shirt(size, large,):
-    print(f"\nI love python is currently printed on {large} size")
-    print(f"{size.title()} size is currently available to purchase")
+def make_shirt(size, large,medium):
+    print(f"\nI love python is currently printed on {large} size.")
+    print(f"\nYou would love the designs on the {medium} size shirt.")
+    print(f"\n{size.title()} size is currently available to purchase.")
 
-make_shirt(size= 'small', large='large')
+make_shirt(size= 'small', large='large', medium='medium')
