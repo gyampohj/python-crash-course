09f8d7d05bd4bdc35556542e56ca4b8d03cb7cda
gyampohj <gyampohj53@gmail.com>
2025-12-01 20:50:12 +0000

    album.py exercise complete

---
+++ try exercises/functions/album.py
@@ -0,0 +1,6 @@
+def make_album(artist_name, album_title):
+    album = {'artist': artist_name, 'title': album_title}
+    return(album)
+
+album = make_album('6lack' , 'East Atlanta Love Letter')
+print(album)
---
+++ try exercises/functions/city_names.py
@@ -0,0 +1,6 @@
+def city_country(city,country):
+    print(f"{city}, {country}")
+
+city_country(city = 'Accra', country = 'Ghana')
+city_country(city= 'Lagos', country= 'Nigeria')
+city_country(city= 'Cape-Town', country= 'South Africa')
