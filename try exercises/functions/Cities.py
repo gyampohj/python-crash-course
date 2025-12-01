7ffa15112ac31dc0744bb9e93cb0d7e638592985
gyampohj <gyampohj53@gmail.com>
2025-12-01 16:25:52 +0000

    cities with default value passed in

--- try exercises/functions/Cities.py
+++ try exercises/functions/Cities.py
@@ -1,4 +1,6 @@
-def describe_city(name_of_city, country):
+def describe_city(name_of_city, country = 'Ghana'):
     print(f"{name_of_city.title()} is in {country}.")
 
-describe_city(name_of_city= 'Accra', country= 'Ghana')
+describe_city(name_of_city= 'Accra')
+describe_city(name_of_city= 'Kumasi')
+describe_city(name_of_city= 'Takoradi')
