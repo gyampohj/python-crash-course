1fb5bb69b7329b725e82f5d45f61cabb1608b250
gyampohj <gyampohj53@gmail.com>
2025-12-09 10:14:40 +0000

    user dictionary remembered working accurately

--- try exercises/files and exceptions/user_dictionary.py
+++ try exercises/files and exceptions/user_dictionary.py
@@ -7,28 +7,37 @@ def get_stored_username(path):
     """Get stored username if available."""
     if path.exists():
         contents = path.read_text()
-        username = json.loads(contents)
-        return username
+        user_dict = json.loads(contents)
+        return user_dict
     else:
         return None
+
+def get_new_user_info(path):
+    username = input("What is your name?:")
+    age = input("Enter you age:")
+    location = input("Enter your location:")
+     
+    user_dict = {
+        'username': username,
+        'age': age,
+        'location': location
+    }
+    contents = json.dumps(user_dict)
+    path.write_text(contents)
+    return user_dict
+    
 
 def greet_user():
     """Greet the user by name."""
     path = Path('username.json')
-    username = get_stored_username(path)
+    user_dict = get_stored_username(path)
 
-    if username:
-        print(f"Welcome back, {username}!")
+    if user_dict:
+        print(f"Welcome back, {user_dict['username']}!")
+        print(f"You are {user_dict['age']} years old")
+        print(f"You stay in {user_dict['location']}")
     else:
-        username = input("What is your name? ")
-        contents = json.dumps(username)
-        path.write_text(contents)
-        print(f"We'll remember you when you come back, {username}!")
+        user_dict = get_new_user_info(path)
+        print(f"We'll remember you when you come back, {user_dict['username']}!")
     
-def encourage_user():
-    """Message of hope"""
-    path = Path('username.json')
-    username = get_stored_username(path)
-    print(f"Smile and have a sunshine day, {username}!")    
-    
-encourage_user()
+greet_user()
